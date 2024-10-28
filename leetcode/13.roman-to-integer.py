bb2e85ad924211edb9fe59cc399e134698d62d93
WMinerva <nicolas.ochoa1697@gmail.com>
2024-10-28 17:48:42 -0400

    lc excz

--- leetcode/13.roman-to-integer.py
+++ leetcode/13.roman-to-integer.py
@@ -1,5 +1,19 @@
 # @leet start
 class Solution:
     def romanToInt(self, s: str) -> int:
-        
+        dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        result = 0
+        prev_value = 0
+
+        for char in reversed(s):
+            value = dic[char]
+            if value < prev_value:
+                result -= value
+            else:
+                result += value
+            prev_value = value
+
+        return result
+
+
 # @leet end
---
+++ leetcode/14.longest-common-prefix.py
@@ -0,0 +1,15 @@
+# @leet start
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        min_length = min(len(s) for s in strs)
+        result = ""
+
+        for i in range(min_length):
+            char = strs[0][i]
+            if all(s[i] == char for s in strs):
+                result += char
+            else:
+                break
+        return result
+        
+# @leet end
