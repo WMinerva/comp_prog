ec9fa58e4afa331888d21a27ab43990e314b51e4
WMinerva <nicolas.ochoa1697@gmail.com>
2024-11-14 21:41:49 -0400

    solved

--- leetcode/20.valid-parentheses.py
+++ leetcode/20.valid-parentheses.py
@@ -1,5 +1,24 @@
 # @leet start
 class Solution:
     def isValid(self, s: str) -> bool:
-        
+        # dic = {"(": 1, ")": -1, "{": 2, "}": -2, "[": 3, "]": -3}
+        # result = 0
+        # for char in s:
+        #     result += dic[char]
+        #
+        # return False if result != 0 else True
+        stack = []
+        dic = {")": "(", "}": "{", "]": "["}
+        for char in s:
+            if char in dic.values():
+                stack.append(char)
+            elif char in dic.keys():
+                if stack == [] or dic[char] != stack.pop():
+                    return False
+            else:
+                return False
+
+        return stack == []
+
+
 # @leet end
